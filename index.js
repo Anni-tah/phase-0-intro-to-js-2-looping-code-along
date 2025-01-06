d122467c0f7563559816d969aa5616f42c8f2d2d
Anne Nabwire <nabanne96@gmail.com>
2025-01-06 20:53:55 +0300

    Lab completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+let names = ['Anne', 'Ethan', 'Steve'];
+function writeCards(names, eventName){
+let message =[];
+for(let i=0; i<names.length; i++){
+ message.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`)
+
+}
+return message;
+
+}
+
+function countDown(){
+  let count =10;
+  while (count>=0){
+    console.log(count);
+    count--;
+  }
+}
